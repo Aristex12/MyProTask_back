d41a19dc7637138ef3fe984c491f0f3a4ce02f30
Aris Maximilian Kuhs <aristex@hotmail.com>
2024-05-09 13:37:25 +0200

    Revert "Creados los enums y modificado la clase User"
    This reverts commit 99db92214c4b556b72d547514142fbf8fa875ac6.

--- myprotask/src/main/java/com/app/myprotask/enums/StatusRequests.java
+++
@@ -1,5 +0,0 @@
-package com.app.myprotask.enums;
-
-public enum StatusRequests {
-
-}
--- myprotask/src/main/java/com/app/myprotask/model/Caracteristic.java
+++ myprotask/src/main/java/com/app/myprotask/model/Caracteristic.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
  
-
+// Hola soy lolo
 @Entity
 @Table(name = "caracteristics")
 public class Caracteristic {
