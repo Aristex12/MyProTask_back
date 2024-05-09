173f2095e5636357c0bf131b1588b35ec6dba330
Aris Maximilian Kuhs <aristex@hotmail.com>
2024-05-09 13:38:05 +0200

    Revert "Prueba"
    This reverts commit 82c2bf450866083f7447e9a8b2e5c308059a0541.

--- myprotask/src/main/java/com/app/myprotask/model/Caracteristic.java
+++ myprotask/src/main/java/com/app/myprotask/model/Caracteristic.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
  
-// Hola
 @Entity
 @Table(name = "caracteristics")
 public class Caracteristic {
