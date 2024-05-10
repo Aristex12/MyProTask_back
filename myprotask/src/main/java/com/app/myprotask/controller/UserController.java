9678b4dedec0a6212ba4735887c1acafdfdafb07
Manuel <manuel.sanz.external@eviden.com>
2024-05-10 14:18:45 +0200

    Create a new API for create users

--- myprotask/src/main/java/com/app/myprotask/controller/UserController.java
+++ myprotask/src/main/java/com/app/myprotask/controller/UserController.java
@@ -2,10 +2,13 @@ package com.app.myprotask.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.myprotask.model.User;
 import com.app.myprotask.model.dao.DAOService;
 
 /**
@@ -30,6 +33,16 @@ public class UserController {
 	}
 	
 	
+	/**
+	 * Method that inserts a new user with their DAS and email
+	 * 
+	 * @author Manuel
+	 * @param user
+	 */
+	@PostMapping(value = "/addUser")
+	public void addUser(@RequestBody User user) {
+		daoS.addUser(user);
+	}
 	
 
 }
