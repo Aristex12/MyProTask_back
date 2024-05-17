e37f9723acbc2c8d6b42a2efb92cc26c3b70c670
Manuel <manuel.sanz.external@eviden.com>
2024-05-17 09:17:40 +0200

    Update method addUser and create updateUser

--- myprotask/src/main/java/com/app/myprotask/controller/UserController.java
+++ myprotask/src/main/java/com/app/myprotask/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.app.myprotask.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.app.myprotask.model.Characteristic;
 import com.app.myprotask.model.User;
 import com.app.myprotask.model.dao.DAOService;
+
+import jakarta.websocket.server.PathParam;
 
 /**
  * @author Alejandro
@@ -79,26 +83,37 @@ public class UserController {
 	 */
 	@PostMapping(value = "/addUser")
 	public void addUser(@RequestBody User userData) {
+
+		List<Characteristic> userCharacteristics = new ArrayList<>();
+
+		// I create a list of characteristics by retrieving them using the obtained ID,
+		// and we add them to the user to be inserted
+		for (Characteristic c : userData.getUserCharacteristics()) {
+			userCharacteristics.add(daoS.displayCharacteristicById(c.getIdCharacteristic()));
+		}
+
 		daoS.addUser(new User(userData.getName(), userData.getLastName(), userData.getPassword(),
-				daoS.getRoleByName("employee"), userData.getUserCharacteristics()));
+				daoS.getRoleByName("employee"), userCharacteristics));
 	}
 
 	/**
 	 * Used in EditUser [ User ]
 	 * 
-	 * Update the list of characteristics of the introduced user with the new obtained
+	 * Updates the introduced user with the new data
 	 * 
 	 * @author Manuel
 	 * @param idUser
 	 * @param userCharacteristics
 	 */
-	@PutMapping(value = "/updateUserCharacteristicsByIdUser")
-	public void updateUserCharacteristicsByIdUser(@RequestParam("idUser") Long idUser,
-			@RequestParam("userCharacteristics") List<Characteristic> userCharacteristics) {
+	@PutMapping(value = "/updateUserByIdUser/{idUser}")
+	public void updateUserByIdUser(@PathVariable("idUser") Long idUser, @RequestBody User userData) {
+
 		User user = daoS.displayUserById(idUser);
 
-		user.setUserCharacteristics(userCharacteristics);
-		
+		user.setCv(userData.getCv());
+		user.setProfilePic(userData.getProfilePic());
+		user.setUserCharacteristics(userData.getUserCharacteristics());
+
 		daoS.updateUser(user);
 	}
 
