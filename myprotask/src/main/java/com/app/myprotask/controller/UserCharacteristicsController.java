a2c44be34d93d225bc6c168439999f68a47427f8
Alejandro <a925980@DESKTOP-LCNNPLF>
2024-05-22 16:23:48 +0200

    update Controllers

--- myprotask/src/main/java/com/app/myprotask/controller/UserCharacteristicsController.java
+++ myprotask/src/main/java/com/app/myprotask/controller/UserCharacteristicsController.java
@@ -1,18 +1,13 @@
 package com.app.myprotask.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.myprotask.model.UserCharacteristic;
 import com.app.myprotask.model.dao.DAOService;
 
 /**
@@ -28,25 +23,26 @@ public class UserCharacteristicsController {
 	DAOService daoS;
 	
 	/**
-	 * 
-	 * @author Alejandro
-	 * @param idUser
-	 * @return list of UserCharacteristics of a specific user
-	 */
-	@GetMapping(value = "/displayUserCharacteristicsByIdUser")
-	public List<UserCharacteristic> displayUserCharacteristicsByIdUser(@RequestParam("idUser") Long idUser) {
-		return daoS.displayUserCharacteristicsByIdUser(idUser);
-	}
-
-	/**
-	 * Update UserCharacteristics with the new list of characteristics
+	 * Add UserCharacteristics with the specific characteristic
 	 * 
 	 * @author Alejandro
 	 * @param userCharacteristics
 	 */
-	@PutMapping(value = "/updateUserCharacteristicByIdUser")
-	public void updateUserCharacteristicByIdUser(@RequestBody List<UserCharacteristic> userCharacteristics) {
+	@PutMapping(value = "/addUserCharacteristicByIdUser")
+	public void addUserCharacteristicByIdUser(@RequestParam("idUser") Long idUser, @RequestParam("idCharacteristic") Long idCharacteristic) {
 		
-		daoS.updateUserCharacteristicByIdUser(userCharacteristics);
+		daoS.addUserCharacteristicByIdUser(idUser, idCharacteristic);
+	}
+	
+	/**
+	 * Delete UserCharacteristics with the specific characteristic
+	 * 
+	 * @author Alejandro
+	 * @param userCharacteristics
+	 */
+	@PutMapping(value = "/addUserCharacteristicByIdUser")
+	public void deleteUserCharacteristicByIdUser(@RequestParam("idUser") Long idUser, @RequestParam("idCharacteristic") Long idCharacteristic) {
+		
+		daoS.deleteUserCharacteristicByIdUser(idUser, idCharacteristic);
 	}
 }
