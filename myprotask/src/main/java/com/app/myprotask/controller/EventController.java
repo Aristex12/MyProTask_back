27b9a743910dbc2588e867f92db047c8b3d8f15e
Manuel <manuel.sanz.external@eviden.com>
2024-05-23 15:41:18 +0200

    Add CrosOrigin in ProjectController

--- myprotask/src/main/java/com/app/myprotask/controller/EventController.java
+++ myprotask/src/main/java/com/app/myprotask/controller/EventController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.myprotask.model.Event;
-import com.app.myprotask.model.Project;
-import com.app.myprotask.model.User;
 import com.app.myprotask.model.dao.DAOService;
 
 /**
